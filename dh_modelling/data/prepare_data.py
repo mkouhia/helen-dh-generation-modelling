2b51ebd5e9ea5f5ee5791a647ed55ffd9cfb1347
Mikko Kouhia <mikko.kouhia@datarangers.fi>
2021-04-12 14:13:46 +0300

    Refactor

--- dh_modelling/data/prepare_data.py
+++ dh_modelling/data/prepare_data.py
@@ -1,23 +1,186 @@
 import logging
 from pathlib import Path
+from typing import Tuple
 
-from pandas import DataFrame, merge
+import pandas as pd
+import requests
+import yaml
+from pandas import DataFrame, DatetimeIndex, concat, merge, read_csv
+from pandas._typing import FilePathOrBuffer
 
-from dh_modelling.data import (
-    intermediate_data_path,
-    load_intermediate,
-    processed_data_path,
-    raw_data_path,
-    save_intermediate,
+HELEN_DATA_URL = (
+    "https://www.helen.fi/globalassets/helen-oy/vastuullisuus/hki_dh_2015_2020_a.csv"
 )
-from dh_modelling.data.helen import HELEN_INTERMEDIATE_FILENAME, GenerationData
-from dh_modelling.data.weather import (
-    WEATHER_INTERMEDIATE_FILENAME,
-    FmiData,
-    fmi_weather_files,
-)
+HELEN_DATA_FILENAME = "hki_dh_2015_2020_a.csv"
 
+HELEN_INTERMEDIATE_FILENAME = "helen_cleaned.feather"
+WEATHER_INTERMEDIATE_FILENAME = "weather_{station}.feather"
 MASTER_INTERMEDIATE_FILENAME = "master.feather"
+
+raw_data_path = (Path(__file__) / "../../../data/raw").resolve()
+intermediate_data_path = (Path(__file__) / "../../../data/intermediate").resolve()
+processed_data_path = (Path(__file__) / "../../../data/processed").resolve()
+
+
+def merge_deep_dict(user, default):
+    if isinstance(user, dict) and isinstance(default, dict):
+        for k, v in default.items():
+            user[k] = v if k not in user else merge_deep_dict(user[k], v)
+    return user
+
+
+default_params = {"prepare": {"split": 0.2}}
+if Path("params.yaml").exists():
+    with open("params.yaml", "r") as fd:
+        params = yaml.safe_load(fd)
+        params = merge_deep_dict(params, default_params)
+else:
+    params = default_params
+
+
+class GenerationData:
+    def __init__(self, raw_file_path: Path = raw_data_path / HELEN_DATA_FILENAME):
+        self.raw_file_path = raw_file_path
+
+    @classmethod
+    def from_url(
+        cls,
+        url: str = HELEN_DATA_URL,
+        raw_file_path: Path = raw_data_path / HELEN_DATA_FILENAME,
+    ):
+        """
+        Get DH data CSV, save to file
+
+        :param url: online location for data
+        :param raw_file_path: local file path for raw data
+        """
+        logging.info(f"Downloading Helen DH data from {url} to {raw_file_path}")
+        r = requests.get(url)
+        with raw_file_path.open("wb") as f:
+            f.write(r.content)
+        return cls(raw_file_path)
+
+    def load_and_clean(self) -> DataFrame:
+        """
+        Load dataset from disk, clean up features
+
+        :param raw_file_path: local file path for raw data
+        :return: Pandas dataframe, with index column 'date_time' and feature column 'dh_MWh'
+        """
+        logging.info("Load Helen raw dataset from CSV, clean up file")
+        df = pd.read_csv(
+            self.raw_file_path,
+            sep=";",
+            decimal=",",
+            parse_dates=["date_time"],
+            dayfirst=True,
+        ).set_index("date_time")
+        df.index = df.index.tz_localize(tz="Europe/Helsinki", ambiguous="infer")
+        return df
+
+
+fmi_weather_files = {
+    "Kaisaniemi": [
+        "csv-f2faff3e-672d-41a0-8bf3-278ab00d8796.csv",
+        "csv-270db165-c228-466b-a438-69caa8c8f075.csv",
+        "csv-c1047dd7-2a2c-4046-bae8-9835353aced4.csv",
+        "csv-9c60edf9-247b-4dc5-8b3d-d50137de4bdc.csv",
+        "csv-5bba8028-c209-48d8-a958-0764f0e927b1.csv",
+        "csv-563b0676-9825-4a09-a9fd-7095ade8dec6.csv",
+        "csv-f6c18948-8fac-45ac-ab2a-bb51173800dc.csv",
+    ]
+}
+
+
+class FmiData:
+    def __init__(self, *raw_file_paths: Path):
+        """
+        Create data loader, from multiple data files mapping to **same weather station**
+
+        :param raw_file_paths: Location of CSV files, downloaded from FMI data service
+        """
+        self.raw_file_paths = raw_file_paths
+
+    def load_and_clean(self) -> DataFrame:
+        """
+        Load data files from disk, clean up features
+        :return: Pandas dataframe, with index column 'datetime'
+        """
+        logging.info("Load and clean up data files")
+        frames = [self.read_file(f) for f in self.raw_file_paths]
+        df = concat(frames)
+        df = df.loc[~df.index.duplicated()]
+
+        df["Ilman lämpötila (degC)"].interpolate(inplace=True)
+
+        return df
+
+    @staticmethod
+    def read_file(filename: FilePathOrBuffer) -> DataFrame:
+        """
+        Read FMI weather data into pandas data frame
+        """
+        logging.info(f"Read FMI weather data: {filename=}")
+        d = read_csv(filename, parse_dates=[["Vuosi", "Kk", "Pv", "Klo"]]).rename(
+            columns={"Vuosi_Kk_Pv_Klo": "date_time"}
+        )
+
+        assert (d["Aikavyöhyke"] == "UTC").all()
+
+        d = d.drop(["Aikavyöhyke"], axis=1).set_index("date_time")
+
+        d.index = d.index.tz_localize("UTC").tz_convert("Europe/Helsinki")
+        return d
+
+
+def train_test_split_time(
+    df: DataFrame, test_size: float = params["prepare"]["split"]
+) -> Tuple[DataFrame, DataFrame]:
+    all_data = df.sort_index()
+    split_idx = int(len(all_data) * (1 - test_size))
+    train = df[:split_idx]
+    test = df[split_idx:]
+    return train, test
+
+
+def save_intermediate(
+    df: DataFrame, intermediate_file_path: Path, reset_datetime_index: bool = True
+):
+    """
+    Save intermediate representation of dataframe to disk
+
+    :param df: dataframe to be saved
+    :param intermediate_file_path: intermediate file location
+    :param reset_datetime_index: reset datetime index to normal column, convert timestamp to UTC
+    """
+    logging.info(f"Save intermediate dataset to {intermediate_file_path}")
+    if reset_datetime_index:
+        index_name: str = df.index.name
+        df = df.reset_index()
+        df[index_name] = DatetimeIndex(df[index_name]).tz_convert("UTC")
+    df.to_feather(intermediate_file_path)
+
+
+def load_intermediate(
+    intermediate_file_path: Path,
+    set_datetime_index: bool = True,
+    date_time_column: str = "date_time",
+    timezone: str = "Europe/Helsinki",
+) -> DataFrame:
+    """
+    Load pre-saved intermediate file from disk
+
+    :param intermediate_file_path: intermediate file location
+    :param set_datetime_index: set DatetimeIndex from column 'date_time_column' with timezone 'timezone'
+    :param date_time_column: column, which should be set as index
+    :param timezone: timezone, at which date_time_column is converted
+    :return: loaded dataframe, with 'date_time' as index
+    """
+    df: DataFrame = pd.read_feather(intermediate_file_path)
+    if set_datetime_index:
+        df.index = DatetimeIndex(df[date_time_column]).tz_convert(timezone)
+        df = df.drop("date_time", axis=1)
+    return df
 
 
 def read_fmi(
@@ -54,18 +217,22 @@ def read_helen(
     return df
 
 
-def merge_helen_fmi() -> DataFrame:
-    df_fmi = read_fmi()[["Ilman lämpötila (degC)"]]
-    df_helen = read_helen()
+def merge_dataframes(
+    df_helen: DataFrame,
+    df_fmi: DataFrame,
+    save_master_data: bool = True,
+    file_path: Path = processed_data_path / MASTER_INTERMEDIATE_FILENAME,
+) -> DataFrame:
 
     df = merge(df_helen, df_fmi, how="left", left_index=True, right_index=True)
 
-    file_path = processed_data_path / MASTER_INTERMEDIATE_FILENAME
-
-    save_intermediate(df, intermediate_file_path=file_path)
+    if save_master_data:
+        save_intermediate(df, intermediate_file_path=file_path)
     return df
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    merge_helen_fmi()
+    df_fmi = read_fmi()[["Ilman lämpötila (degC)"]]
+    df_helen = read_helen()
+    merge_dataframes(df_helen=df_helen, df_fmi=df_fmi)
--- tests/data/test_prepare_data.py
+++ tests/data/test_prepare_data.py
@@ -1,9 +1,18 @@
 from io import StringIO
+from pathlib import Path
 
 from pandas import DataFrame, DatetimeIndex, read_csv, to_datetime
 from pandas._testing import assert_frame_equal
 
-from dh_modelling.data.prepare_data import merge_helen_fmi, read_fmi, read_helen
+from dh_modelling.data.prepare_data import (
+    FmiData,
+    GenerationData,
+    load_intermediate,
+    merge_dataframes,
+    read_fmi,
+    read_helen,
+    save_intermediate,
+)
 
 
 def test_read_fmi_intermediate(tmp_path, mocker):
@@ -26,7 +35,7 @@ def test_read_fmi_raw(tmp_path, mocker):
     expected = DataFrame({"x": [1, 2]})
 
     mocker.patch(
-        "dh_modelling.data.weather.FmiData.load_and_clean", return_value=expected
+        "dh_modelling.data.prepare_data.FmiData.load_and_clean", return_value=expected
     )
     mocker.patch("dh_modelling.data.prepare_data.save_intermediate")
 
@@ -55,7 +64,8 @@ def test_read_helen_raw(tmp_path, mocker):
     expected = DataFrame({"x": [1, 2]})
 
     mocker.patch(
-        "dh_modelling.data.helen.GenerationData.load_and_clean", return_value=expected
+        "dh_modelling.data.prepare_data.GenerationData.load_and_clean",
+        return_value=expected,
     )
     mocker.patch("dh_modelling.data.prepare_data.save_intermediate")
 
@@ -117,10 +127,137 @@ def test_merge_helen_fmi(mocker):
     ).tz_convert("Europe/Helsinki")
     expected = expected.drop("date_time", axis=1)
 
-    mocker.patch("dh_modelling.data.prepare_data.read_fmi", return_value=df_fmi)
-    mocker.patch("dh_modelling.data.prepare_data.read_helen", return_value=df_helen)
     mocker.patch("dh_modelling.data.prepare_data.save_intermediate")
 
-    received = merge_helen_fmi()
+    received = merge_dataframes(
+        df_helen=df_helen, df_fmi=df_fmi[["Ilman lämpötila (degC)"]]
+    )
 
     assert_frame_equal(received, expected)
+
+
+def test_from_url(tmp_path, requests_mock):
+    test_file_path: Path = tmp_path / "test.csv"
+
+    content = """date_time;dh_MWh
+    29.3.2015 2:00;919,913
+    29.3.2015 4:00;913,885
+    """
+
+    test_url = "http://240.0.0.0/download/test.csv"
+    requests_mock.get(test_url, text=content)
+
+    GenerationData.from_url(url=test_url, raw_file_path=test_file_path)
+    assert test_file_path.exists()
+
+
+def test_load_and_clean(tmp_path):
+    expected = DataFrame(
+        data={
+            "date_time": [
+                "2015-03-29 02:00:00+02:00",
+                "2015-03-29 04:00:00+03:00",
+                "2015-03-29 05:00:00+03:00",
+                "2020-10-25 02:00:00+03:00",
+                "2020-10-25 03:00:00+03:00",
+                "2020-10-25 03:00:00+02:00",
+            ],
+            "dh_MWh": [
+                919.913,
+                913.885,
+                908.093,
+                848.808,
+                851.583,
+                842.317,
+            ],
+        }
+    )
+    expected.index = DatetimeIndex(
+        to_datetime(expected["date_time"], utc=True)
+    ).tz_convert("Europe/Helsinki")
+    expected = expected.drop("date_time", axis=1)
+
+    raw_file_path = tmp_path / "test.csv"
+
+    with raw_file_path.open("w") as f:
+        content = """date_time;dh_MWh
+29.3.2015 2:00;919,913
+29.3.2015 4:00;913,885
+29.3.2015 5:00;908,093
+25.10.2020 2:00;848,808
+25.10.2020 3:00;851,583
+25.10.2020 3:00;842,317
+"""
+        f.write(content)
+
+    g = GenerationData(raw_file_path)
+    received: DataFrame = g.load_and_clean()
+
+    assert_frame_equal(received, expected)
+
+
+def test_load_and_clean_fmi(tmp_path):
+    file_path_1 = tmp_path / "test1.csv"
+    with file_path_1.open("w", encoding="utf8") as f:
+        content = """Vuosi,Kk,Pv,Klo,Aikavyöhyke,Pilvien määrä (1/8),Ilmanpaine (msl) (hPa),Sademäärä (mm),Suhteellinen kosteus (%),Sateen intensiteetti (mm/h),Lumensyvyys (cm),Ilman lämpötila (degC),Kastepistelämpötila (degC),Näkyvyys (m),Tuulen suunta (deg),Puuskanopeus (m/s),Tuulen nopeus (m/s)
+2014,12,1,00:00,UTC,5,1033.2,0,92,0,0,-2.9,-4,,341,1.8,1.3
+2014,12,1,01:00,UTC,0,1032.8,0,94,0,0,-4,-4.8,,351,1.9,1.3
+2014,12,1,02:00,UTC,3,1032.8,0,96,0,0,-4.2,-4.8,,11,2.6,1.6
+2014,12,1,03:00,UTC,7,1032.7,0,94,0,0,-3.1,-3.9,,3,2.4,1.5"""
+
+        f.write(content)
+
+    file_path_2 = tmp_path / "test2.csv"
+    with file_path_2.open("w", encoding="utf8") as f:
+        content = """Vuosi,Kk,Pv,Klo,Aikavyöhyke,Pilvien määrä (1/8),Ilmanpaine (msl) (hPa),Sademäärä (mm),Suhteellinen kosteus (%),Sateen intensiteetti (mm/h),Lumensyvyys (cm),Ilman lämpötila (degC),Kastepistelämpötila (degC),Näkyvyys (m),Tuulen suunta (deg),Puuskanopeus (m/s),Tuulen nopeus (m/s)
+2014,12,1,03:00,UTC,7,1032.7,0,94,0,0,-3.1,-3.9,,3,2.4,1.5
+2014,12,1,04:00,UTC,7,1032.7,0,92,0,0,-3.1,-4.2,,332,2.8,2.1
+2014,12,1,05:00,UTC,7,1032.5,0,91,0,0,-3.1,-4.5,,337,2.8,1.9
+2014,12,1,06:00,UTC,7,1032.7,0,89,0,0,-3.2,-4.7,,350,3.3,2.7"""
+
+        f.write(content)
+
+    expected_content = """date_time,Pilvien määrä (1/8),Ilmanpaine (msl) (hPa),Sademäärä (mm),Suhteellinen kosteus (%),Sateen intensiteetti (mm/h),Lumensyvyys (cm),Ilman lämpötila (degC),Kastepistelämpötila (degC),Näkyvyys (m),Tuulen suunta (deg),Puuskanopeus (m/s),Tuulen nopeus (m/s)
+    2014-12-01 00:00+00:00,5,1033.2,0,92,0,0,-2.9,-4,,341,1.8,1.3
+    2014-12-01 01:00+00:00,0,1032.8,0,94,0,0,-4,-4.8,,351,1.9,1.3
+    2014-12-01 02:00+00:00,3,1032.8,0,96,0,0,-4.2,-4.8,,11,2.6,1.6
+    2014-12-01 03:00+00:00,7,1032.7,0,94,0,0,-3.1,-3.9,,3,2.4,1.5
+    2014-12-01 04:00+00:00,7,1032.7,0,92,0,0,-3.1,-4.2,,332,2.8,2.1
+    2014-12-01 05:00+00:00,7,1032.5,0,91,0,0,-3.1,-4.5,,337,2.8,1.9
+    2014-12-01 06:00+00:00,7,1032.7,0,89,0,0,-3.2,-4.7,,350,3.3,2.7"""
+    expected = read_csv(StringIO(expected_content), parse_dates=["date_time"])
+
+    expected.index = DatetimeIndex(expected["date_time"]).tz_convert("Europe/Helsinki")
+    expected = expected.drop("date_time", axis=1)
+
+    data = FmiData(file_path_1, file_path_2)
+    received: DataFrame = data.load_and_clean()
+
+    assert_frame_equal(received, expected)
+
+
+def test_save_and_load_intermediate(tmp_path):
+    original = DataFrame(
+        data={
+            "date_time": [
+                "2015-03-29 00:00:00+02:00",
+                "2015-03-29 01:00:00+02:00",
+            ],
+            "dh_MWh": [
+                958.673,
+                930.965,
+            ],
+        }
+    )
+    original.index = DatetimeIndex(
+        to_datetime(original["date_time"], utc=True)
+    ).tz_convert("Europe/Helsinki")
+    original = original.drop("date_time", axis=1)
+
+    fpath = tmp_path / "test.feather"
+    save_intermediate(original, intermediate_file_path=fpath)
+    assert fpath.exists()
+
+    received: DataFrame = load_intermediate(intermediate_file_path=fpath)
+
+    assert_frame_equal(original, received)
