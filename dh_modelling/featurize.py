e8d968c12dacbf8217ce5303e3639be3f4ed78dc
Mikko Kouhia <mikko.kouhia@datarangers.fi>
2021-04-14 11:41:54 +0300

    Add time-based features

--- dh_modelling/featurize.py
+++ dh_modelling/featurize.py
@@ -1,8 +1,11 @@
 import argparse
 import logging
+from datetime import timezone
 from pathlib import Path
 
-from pandas import DataFrame
+import holidays
+import numpy as np
+from pandas import DataFrame, DatetimeIndex, Timedelta, Timestamp
 
 from .helpers import load_intermediate, save_intermediate
 
@@ -14,7 +17,33 @@ def featurize(df: DataFrame) -> DataFrame:
     :param df: input dataframe
     :return: modified dataframe
     """
+    assert isinstance(df.index, DatetimeIndex)
+    df["hour_of_day"] = df.index.hour
+    df["day_of_week"] = df.index.day_of_week
+    df["day_of_year"] = df.index.dayofyear
+    df["epoch_seconds"] = (
+        df.index.tz_convert(tz=timezone.utc) - Timestamp("1970-01-01", tz=timezone.utc)
+    ) // Timedelta("1 second")
+    df["is_business_day"] = is_business_day(df.index).astype(np.int32)
     return df
+
+
+def is_business_day(idx: DatetimeIndex, country: str = "Finland") -> np.ndarray:
+    """
+    Determine if timestamps are within business days
+
+    :param idx: datetime index
+    :param country: string representation of country
+    :return: boolean array of the same shape as 'idx', containing True for each valid business day
+    """
+    holiday_calendar = holidays.CountryHoliday(country)
+    min_date = idx.min().date()
+    max_date = idx.max().date()
+    holidays_in_range: list = holiday_calendar[min_date:max_date]
+
+    bcal = np.busdaycalendar(holidays=np.array(holidays_in_range, dtype=np.datetime64))
+    idx_dates: np.ndarray = np.array(idx.date, dtype=np.datetime64)
+    return np.is_busday(idx_dates, busdaycal=bcal)
 
 
 if __name__ == "__main__":
--- tests/test_featurize.py
+++ tests/test_featurize.py
@@ -1,12 +1,41 @@
-from pandas import DataFrame
+import numpy as np
+from pandas import DataFrame, DatetimeIndex, to_datetime
 from pandas.testing import assert_frame_equal
 
 from dh_modelling.featurize import featurize
 
 
 def test_featurize():
-    df_input = DataFrame({"x": [1, 2]})
-    expected = DataFrame({"x": [1, 2]})
+    df_input = DataFrame(
+        data={
+            "date_time": [
+                "2014-12-01 00:00+00:00",
+                "2014-12-01 01:00+00:00",
+                "2014-12-01 02:00+00:00",
+                "2014-12-01 03:00+00:00",
+            ],
+            "dh_MWh": [
+                919.913,
+                913.885,
+                908.093,
+                848.808,
+            ],
+            "Ilman lämpötila (degC)": [-2.9, -4, -4.2, -3.1],
+        }
+    )
+    df_input.index = DatetimeIndex(
+        to_datetime(df_input["date_time"], utc=True)
+    ).tz_convert("Europe/Helsinki")
+    df_input = df_input.drop("date_time", axis=1)
+
+    expected = df_input.copy()
+    expected["hour_of_day"] = np.array([2, 3, 4, 5]).astype(np.int64)
+    expected["day_of_week"] = np.array([0, 0, 0, 0]).astype(np.int64)
+    expected["day_of_year"] = np.array([335, 335, 335, 335]).astype(np.int64)
+    expected["epoch_seconds"] = np.array(
+        [1417392000, 1417395600, 1417399200, 1417402800]
+    ).astype(np.int64)
+    expected["is_business_day"] = np.array([1, 1, 1, 1])
 
     received: DataFrame = featurize(df_input)
 
